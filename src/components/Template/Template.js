f6301cc666e1194f6b1b18de8eb2241562ff8454
Maria Joaquina <morenoaulaciomariajoaquina@gmail.com>
2024-01-18 12:40:05 -0400

    información terminada

--- src/components/Template/Template.js
+++ src/components/Template/Template.js
@@ -2,24 +2,42 @@ import React from 'react';
 import './Template.css';
 
 // Components
+import Button from '../Button/Button';
 import Card from '../Card/Card';
-import Information from '../Information/Information';
 import Navbar from '../Navbar/Navbar';
 
 function Template( props ) {
   return (
     <div className='container'>
 
+      {/* Componente de la barra de navegación */}
       <Navbar />
 
+      {/* Botón de volver */}
       <div className='go-back'>
         <i className='fa-solid fa-arrow-left'></i>
       </div>
 
-      <Information />
+      {/* Seción de información */}
+      <div className='Information spaces'>
+        <div className='left-section'>
+          <p>Mentoría / Mateo De la Torre</p>
+          <p>ADMINISTRACIÓN</p>
+        </div>
+        <div className='center-section'>
+          <p>Rol: <strong>NombreDelRol</strong></p>
+        </div>
+        <div className='right-section'>
+          <p><strong>Última mentoría</strong></p>
+          <p>30/06/2023 a las 3:40 am</p>
+          <div>
+            <Button title='Editar Rol' />
+            <Button title='Terminar Mentoría' />
+          </div>
+        </div>
+      </div>
       
       <div className='center-buttons'>
-        
         <button>Entregables</button>
         <button>Funciones</button>
         <button>Tareas</button>
